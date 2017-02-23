37937d0ab5a8330fa7c4d8bcf6d6adb60589a741
K1532 <K1532@OHJE-330-WS-24>
2017-02-23 10:57:10 +0200

    dsadsa

--- Demo_01/Teht_01/MainPage.xaml.cs
+++ Demo_01/Teht_01/MainPage.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Demo_01
+namespace Teht_01
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -25,7 +25,6 @@ namespace Demo_01
         public MainPage()
         {
             this.InitializeComponent();
-            
         }
     }
 }
